eaea491481a0e872a46e71b800684403e6d3b9cd
kittiposete <96778671+kittiposete@users.noreply.github.com>
2024-07-26 18:22:04 +0700

    refactor

--- plugins/obs-websocket/src/requesthandler/RequestHandlerEte.cpp
+++ plugins/obs-websocket/src/requesthandler/RequestHandlerEte.cpp
@@ -2,32 +2,6 @@
 #include "RequestHandler.h"
 
 RequestResult RequestHandler::ShutdownOBS(const Request &) {
-//    obs_shutdown();
-
-//#ifdef _WIN32
-//    bool disableAudioDucking =
-//        config_get_bool(globalConfig, "Audio", "DisableAudioDucking");
-//    if (disableAudioDucking)
-//        DisableAudioDucking(false);
-//#else
-//    delete snInt;
-//    close(sigintFd[0]);
-//    close(sigintFd[1]);
-//#endif
-//
-//#ifdef __APPLE__
-//    bool vsyncDisabled =
-//            config_get_bool(globalConfig, "Video", "DisableOSXVSync");
-//    bool resetVSync =
-//            config_get_bool(globalConfig, "Video", "ResetOSXVSyncOnExit");
-//    if (vsyncDisabled && resetVSync)
-//        EnableOSXVSync(true);
-//#endif
-//
-//    os_inhibit_sleep_set_active(sleepInhibitor, false);
-//    os_inhibit_sleep_destroy(sleepInhibitor);
-//
-//    if (libobs_initialized)
 
         obs_shutdown();
 
